c3bf7085c93a60b6e5db4a1b89df367b85171b1b
Petre Vane <PetreVane@users.noreply.github.com>
2020-06-25 20:48:02 +0200

    Refactored & removed navigationView

--- MealPlan/Screens/MainScreen.swift
+++ MealPlan/Screens/MainScreen.swift
@@ -10,8 +10,7 @@ import SwiftUI
 
 struct MainScreen: View {
     
-    @State var date = Date()
-    @State private var isActive: Bool = false
+    @State private var date = Date()
     private var customFont = "Avenir Next"
     
     private var dateFormatter: DateFormatter {
@@ -21,78 +20,93 @@ struct MainScreen: View {
     }
     
     var body: some View {
-        NavigationView {
-            ScrollView(.vertical, showsIndicators: false) {
-                    VStack(alignment: .center, spacing: 15) {
-                        Text("Today is \(dateFormatter.string(from: date))")
-                            .font(.custom(customFont, size: 20))
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+            VStack(alignment: .center, spacing: 25) {
+                
+                Spacer(minLength: 15)
+                Text("Today is \(dateFormatter.string(from: date))")
+                    .font(.custom(customFont, size: 20))
+                    .fontWeight(.ultraLight)
+                    .padding(.all, -25)
+                    .border(Color.black, width: 0.3)
+                
+                Text("Hi beatiful")
+                    .fontWeight(.light)
+                    .padding()
+                    .font(.custom(customFont, size: 25))
+                
+                // Weight textfield & button
+                HStack {
+                    CustomTextField(placeholderText: "Your weight today")
+                        .fixedSize()
+                    
+                    Button(action: { print("Saving your weight")}) {
+                        Text("Update weight")
+                            .font(.custom(customFont, size: 15))
                             .fontWeight(.ultraLight)
-                            .padding(.all, -25)
-                            .border(Color.black, width: 0.3)
-                        
-                        Text("Hi beatiful")
-                            .fontWeight(.light)
-                            .padding()
-                            .font(.custom(customFont, size: 25))
-                        
-                        // Weight textfield & button
-                        HStack {
-                            CustomTextField(placeholderText: "Your weight today")
-                                .fixedSize()
-                            
-                            Button(action: { print("Saving your weight")}) {
-                                Text("Update weight")
-                                    .font(.custom(customFont, size: 15))
-                                    .fontWeight(.ultraLight)
-                                    .cornerRadius(35)
-                                    .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
-                                    .border(Color.gray, width: 0.5)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                        
-                        // Height textfield & button
-                        HStack {
-                            CustomTextField(placeholderText: "Your height today")
-                                .fixedSize()
-                            
-                            Button(action: { print("Saving your height")}) {
-                                Text("Update height")
-                                    .font(.custom(customFont, size: 15))
-                                    .fontWeight(.ultraLight)
-                                    .cornerRadius(35)
-                                    .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
-                                    .border(Color.gray, width: 0.5)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                        
-                        Text("Your meal plan")
-                            .font(.custom(customFont, size: 35))
-                            .fontWeight(.ultraLight)
-                        
-                        // Links
-                        NavigationLink(destination: BreakfastScreen()) {
-                            CustomTextView(title: "Breakfast")
-                        }
-                        
-                        NavigationLink(destination: LunchScreen()) {
-                            CustomTextView(title: "Lunch    ")
-                        }
-                        
-                        NavigationLink(destination: DinnerScreen()) {
-                            CustomTextView(title: "Dinner   ")
-                        }
+                            .cornerRadius(35)
+                            .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
+                            .border(Color.gray, width: 0.5)
+                            .foregroundColor(.gray)
                     }
                 }
+                
+                // Height textfield & button
+                HStack {
+                    CustomTextField(placeholderText: "Your height today")
+                        .fixedSize()
+                    
+                    Button(action: { print("Saving your height")}) {
+                        Text("Update height")
+                            .font(.custom(customFont, size: 15))
+                            .fontWeight(.ultraLight)
+                            .cornerRadius(35)
+                            .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
+                            .border(Color.gray, width: 0.5)
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                Text("Your meal plan")
+                    .font(.custom(customFont, size: 35))
+                    .fontWeight(.ultraLight)
+                
+                BottomStack()
             }
-        .statusBar(hidden: false)
-        .navigationBarBackButtonHidden(true)
+        }
     }
 }
 
 struct MainScreen_Previews: PreviewProvider {
     static var previews: some View {
         MainScreen()
+    }
+}
+
+struct BottomStack: View {
+    
+    @State private var showBreakfastScreen = false
+    @State private var showLunchScreen = false
+    @State private var showDinnerScreen = false
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Button(action: {
+                self.showBreakfastScreen.toggle()
+            }) { CustomTextView(title: "Breakfast")}
+                .sheet(isPresented: $showBreakfastScreen) { BreakfastScreen() }
+            
+            Button(action: {
+                self.showLunchScreen.toggle()
+            }) { CustomTextView(title: "Lunch    ")}
+                .sheet(isPresented: $showLunchScreen) { LunchScreen() }
+            
+            Button(action: {
+                self.showDinnerScreen.toggle()
+            }) { CustomTextView(title: "Dinner   ")}
+                .sheet(isPresented: $showDinnerScreen) { DinnerScreen() }
+        }
     }
 }
