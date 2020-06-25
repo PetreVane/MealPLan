c2f484a10ced1b0279a1c1e9228fe6e459891dc3
Petre Vane <PetreVane@users.noreply.github.com>
2020-06-25 10:39:18 +0200

    Initial Commit

---
+++ MealPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealPlan
+//
+//  Created by Petre Vane on 25/06/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
