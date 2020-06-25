11d213b3a962fca0ad556847b1cd28a29f3d35b6
Petre Vane <PetreVane@users.noreply.github.com>
2020-06-25 15:12:10 +0200

    Added a dummy screen; implemented dummy navigation link

--- MealPlan/Screens/ChallengeScreen.swift
+++ MealPlan/Screens/ChallengeScreen.swift
@@ -1,6 +1,6 @@
 //
 //  LaunchScreen.swift
-//  Tracky
+//  MealPlan
 //
 //  Created by Petre Vane on 16/06/2020.
 //  Copyright © 2020 Petre Vane. All rights reserved.
@@ -10,28 +10,28 @@ import SwiftUI
 
 
 struct ChallengeScreen: View {
+
     var body: some View {
         
-        VStack(alignment: .center, spacing: 150) {
-            
-            Text("My meal plan")
-                .font(.custom("Avenir Next", size: 55))
-                .fontWeight(.ultraLight)
-                .lineLimit(1)
+        NavigationView {
+            VStack(alignment: .center, spacing: 150) {
                 
-            
-            Button(action: {
-                self.instantiateTabBar()
-
-            }) {
-                Text("Accept Challenge")
-                    .font(.custom("Avenir Next", size: 25))
-                    .fontWeight(.thin)
-                    .foregroundColor(.black)
-                    .padding()
-                    .border(Color.black)
+                Text("My meal plan")
+                    .font(.custom("Avenir Next", size: 55))
+                    .fontWeight(.ultraLight)
+                    .lineLimit(1)
+                
+                NavigationLink(destination: DetailedView()) {
+                    Text("Accept Challenge")
+                        .font(.custom("Avenir Next", size: 25))
+                        .fontWeight(.thin)
+                        .foregroundColor(.black)
+                        .padding()
+                        .border(Color.black)
+                }
             }
-        }
+        }.navigationBarTitle("Navigation")
+        .labelsHidden()
     }
     
     func instantiateTabBar() {
---
+++ MealPlan/Screens/DetailedView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailedView.swift
+//  MealPlan
+//
+//  Created by Petre Vane on 25/06/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailedView: View {
+    var body: some View {
+        Text("This is a detailed view!")
+    }
+}
+
+struct DetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedView()
+    }
+}
