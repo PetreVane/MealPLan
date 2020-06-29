2b577d00d725a45462498c7e67be2e763235c378
Petre Vane <PetreVane@users.noreply.github.com>
2020-06-29 12:03:52 +0200

    Refactored view; added tabBar view

--- MealPlan/Screens/MainScreen.swift
+++ MealPlan/Screens/MainScreen.swift
@@ -10,9 +10,60 @@ import SwiftUI
 
 struct MainScreen: View {
     
-    @State private var date = Date()
-    private var customFont = "Avenir Next"
+    var body: some View {
+        TabView {
+            MainSubView()
+                .tabItem {
+                    Image(systemName: "sun.haze.fill").renderingMode(.original)
+                    Text(verbatim: "Daily records")
+                }.tag(0)
+                
+        
+            LogOutScreen()
+                .tabItem {
+                    Image(systemName: "star.circle.fill").renderingMode(.original)
+                    Text(verbatim: "Your progress")
+            }.tag(1)
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
+
+struct BottomStack: View {
     
+    @State private var showBreakfastScreen = false
+    @State private var showLunchScreen = false
+    @State private var showDinnerScreen = false
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Button(action: {
+                self.showBreakfastScreen.toggle()
+            }) { CustomTextView(title: "Breakfast")}
+                .sheet(isPresented: $showBreakfastScreen) { BreakfastScreen() }
+            
+            Button(action: {
+                self.showLunchScreen.toggle()
+            }) { CustomTextView(title: "Lunch    ")}
+                .sheet(isPresented: $showLunchScreen) { LunchScreen() }
+            
+            Button(action: {
+                self.showDinnerScreen.toggle()
+            }) { CustomTextView(title: "Dinner   ")}
+                .sheet(isPresented: $showDinnerScreen) { DinnerScreen() }
+        }
+    }
+}
+
+struct TopStackView: View {
+    
+    private var customFont = "Avenit Next"
+    private var date = Date()
     private var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
@@ -20,22 +71,32 @@ struct MainScreen: View {
     }
     
     var body: some View {
-        
+        VStack {
+            Spacer(minLength: 15)
+            Text("Today is \(dateFormatter.string(from: date))")
+                .font(.custom(customFont, size: 20))
+                .fontWeight(.ultraLight)
+                .padding(.all, -25)
+                .border(Color.black, width: 0.3)
+            
+            Text("Hi beatiful")
+                .fontWeight(.light)
+                .padding()
+                .font(.custom(customFont, size: 25))
+        }
+    }
+}
+
+struct MainSubView: View {
+    
+    private var customFont = "Avenir Next"
+    
+    var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             
             VStack(alignment: .center, spacing: 25) {
-                
-                Spacer(minLength: 15)
-                Text("Today is \(dateFormatter.string(from: date))")
-                    .font(.custom(customFont, size: 20))
-                    .fontWeight(.ultraLight)
-                    .padding(.all, -25)
-                    .border(Color.black, width: 0.3)
-                
-                Text("Hi beatiful")
-                    .fontWeight(.light)
-                    .padding()
-                    .font(.custom(customFont, size: 25))
+                // Date and hello message
+                TopStackView()
                 
                 // Weight textfield & button
                 HStack {
@@ -69,44 +130,13 @@ struct MainScreen: View {
                     }
                 }
                 
+                // Middle textView
                 Text("Your meal plan")
                     .font(.custom(customFont, size: 35))
                     .fontWeight(.ultraLight)
                 
                 BottomStack()
             }
-        }
-    }
-}
-
-struct MainScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        MainScreen()
-    }
-}
-
-struct BottomStack: View {
-    
-    @State private var showBreakfastScreen = false
-    @State private var showLunchScreen = false
-    @State private var showDinnerScreen = false
-    
-    var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Button(action: {
-                self.showBreakfastScreen.toggle()
-            }) { CustomTextView(title: "Breakfast")}
-                .sheet(isPresented: $showBreakfastScreen) { BreakfastScreen() }
-            
-            Button(action: {
-                self.showLunchScreen.toggle()
-            }) { CustomTextView(title: "Lunch    ")}
-                .sheet(isPresented: $showLunchScreen) { LunchScreen() }
-            
-            Button(action: {
-                self.showDinnerScreen.toggle()
-            }) { CustomTextView(title: "Dinner   ")}
-                .sheet(isPresented: $showDinnerScreen) { DinnerScreen() }
         }
     }
 }
