cdf23a26e39070f4968270b94a69956da88a8fbb
Petre Vane <petre.vane@gmail.com>
2020-07-14 10:48:24 +0200

    User input saved into instance of FoodIntake model

--- MealPlan/Views/DinnerScreen.swift
+++ MealPlan/Views/DinnerScreen.swift
@@ -9,36 +9,39 @@
 import SwiftUI
 
 struct DinnerScreen: View {
-
+    
+    @State private var proteins = ""
+    @State private var grains = ""
+    @State private var vegetables = ""
+    @State private var salad = ""
+    @State private var fat = ""
+    
+    
+    let foodIntakeController = FoodIntakeController.sharedInstance
+    
+    
     var customFont = "Avenir Next"
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
                 Spacer(minLength: 15)
-                GenericScreen(title: "Dinner", firstRow: ("1 Protein", "Type your protein intake here"), secondRow: ("6 oz. Vegetables", "Type your Vegetables intake here"), thirdRow: ("Type your Salad intake here", "Type your Salad intake here"))
                 
-                VStack(alignment: .leading, spacing: 15) {
-                    HStack {
-                        Circle()
-                            .fixedSize()
-                            .foregroundColor(.gray)
-                        
-                        Text("8 oz. Salad")
-                            .font(.custom(customFont, size: 25))
-                            .fontWeight(.thin)
-                            .italic()
-                    }
-                    
-                    CustomTextField(placeholderText: "Type your Salad intake here", customFont: customFont)
-                }.padding()
+                GenericScreen(title: "Dinner", firstRow: ("Proteins", "Type your protein intake here"), secondRow: (title: "Grains", placeholder: "Type Grains intake here"), thirdRow: ("Vegetables", "Type your Vegetables intake here"), fourthRow: ("Salad", "Type your Salad intake here"), fifthRow: ("Fat", "Type yoir Fat intake here"), firstTextField: $proteins, secondTextField: $grains, thirdTextField: $vegetables, fourthTextField: $salad, fifthTextField: $fat)
                 
                 Spacer(minLength: 50)
                 
                 CustomButton(title: "Save dinner") {
                     print("Saving dinner details now ...")
+                    self.saveFoodIntake()
                 }
             }
         }
+    }
+    
+    func saveFoodIntake() {
+        let foodIntake = FoodIntake(proteins: proteins, grains: grains, fruits: "", vegetables: vegetables, salad: salad, fats: fat)
+        foodIntakeController.add(foodIntake)
+        foodIntakeController.checkStatus()
     }
 }
 
