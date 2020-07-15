867ae60a2bc95ffb7157a3339ed4c8a1de0807fa
Petre Vane <petre.vane@gmail.com>
2020-07-15 12:45:09 +0200

    Added form view with integrated segmented control

---
+++ MealPlan/Custom Views/CustomSegmentedControl.swift
@@ -0,0 +1,45 @@
+//
+//  CustomSegmentedControl.swift
+//  MealPlan
+//
+//  Created by Petre Vane on 14/07/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomSegmentedControl: View {
+    @State private var selection: [String] = []
+    @State var timeSelection = 0
+    
+    private var timeOfDay = ["Breakfast", "Lunch", "Dinner"]
+    
+    
+    var body: some View {
+        NavigationView {
+            
+            Form {
+                Section {
+                    Picker("Options", selection: $timeSelection) {
+                        ForEach(0 ..< timeOfDay.count) { index in
+                            Text(self.timeOfDay[index]).tag(index)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    NavigationLink("Press me", destination: MultipleSelectionList())
+
+                }
+                
+                Text("You've selected: \(timeOfDay[timeSelection])")
+            }.navigationBarTitle("Form")
+        }
+    }
+}
+
+struct CustomSegmentedControl_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSegmentedControl()
+    }
+}
