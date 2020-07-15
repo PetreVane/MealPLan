422c68d43bef249dca6e93a307a7399482000527
Petre Vane <petre.vane@gmail.com>
2020-07-15 20:10:40 +0200

    Commented out / added files to version control

--- MealPlan/Views/DinnerScreen.swift
+++ MealPlan/Views/DinnerScreen.swift
@@ -8,45 +8,45 @@
 
 import SwiftUI
 
-struct DinnerScreen: View {
-    
-    @State private var proteins = ""
-    @State private var grains = ""
-    @State private var vegetables = ""
-    @State private var salad = ""
-    @State private var fat = ""
-    
-    
-    let foodIntakeController = FoodIntakeController.sharedInstance
-    
-    
-    var customFont = "Avenir Next"
-    var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack {
-                Spacer(minLength: 15)
-                
-                GenericScreen(title: "Dinner", firstRow: ("Proteins", "Type your protein intake here"), secondRow: (title: "Grains", placeholder: "Type Grains intake here"), thirdRow: ("Vegetables", "Type your Vegetables intake here"), fourthRow: ("Salad", "Type your Salad intake here"), fifthRow: ("Fat", "Type yoir Fat intake here"), firstTextField: $proteins, secondTextField: $grains, thirdTextField: $vegetables, fourthTextField: $salad, fifthTextField: $fat)
-                
-                Spacer(minLength: 50)
-                
-                CustomButton(title: "Save dinner") {
-                    print("Saving dinner details now ...")
-//                    self.saveFoodIntake()
-                }
-            }
-        }
-    }
-    
-//    func saveFoodIntake() {
-//        let foodIntake = FoodIntake(proteins: proteins, grains: grains, fruits: "", vegetables: vegetables, salad: salad, fats: fat)
-//        foodIntakeController.add(foodIntake)
-//        foodIntakeController.checkStatus()
+//struct DinnerScreen: View {
+//    
+//    @State private var proteins = ""
+//    @State private var grains = ""
+//    @State private var vegetables = ""
+//    @State private var salad = ""
+//    @State private var fat = ""
+//    
+//    
+//    let foodIntakeController = FoodIntakeController.sharedInstance
+//    
+//    
+//    var customFont = "Avenir Next"
+//    var body: some View {
+//        ScrollView(.vertical, showsIndicators: false) {
+//            VStack {
+//                Spacer(minLength: 15)
+//                
+////                GenericScreen(title: "Dinner", firstRow: ("Proteins", "Type your protein intake here"), secondRow: (title: "Grains", placeholder: "Type Grains intake here"), thirdRow: ("Vegetables", "Type your Vegetables intake here"), fourthRow: ("Salad", "Type your Salad intake here"), fifthRow: ("Fat", "Type yoir Fat intake here"), firstTextField: $proteins, secondTextField: $grains, thirdTextField: $vegetables, fourthTextField: $salad, fifthTextField: $fat)
+//                
+//                Spacer(minLength: 50)
+//                
+//                CustomButton(title: "Save dinner") {
+//                    print("Saving dinner details now ...")
+////                    self.saveFoodIntake()
+//                }
+//            }
+//        }
 //    }
-}
-
-struct DinnerScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        DinnerScreen()
-    }
-}
+//    
+////    func saveFoodIntake() {
+////        let foodIntake = FoodIntake(proteins: proteins, grains: grains, fruits: "", vegetables: vegetables, salad: salad, fats: fat)
+////        foodIntakeController.add(foodIntake)
+////        foodIntakeController.checkStatus()
+////    }
+//}
+//
+//struct DinnerScreen_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DinnerScreen()
+//    }
+//}
