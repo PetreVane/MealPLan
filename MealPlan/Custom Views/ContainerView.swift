5c444ee0a050cdb75e1e5b58524cb1a9c353f432
Petre Vane <petre.vane@gmail.com>
2020-07-24 17:21:38 +0200

    Removed files

---
+++ MealPlan/Custom Views/ContainerView.swift
@@ -0,0 +1,24 @@
+//
+//  ContainerView.swift
+//  MealPlan
+//
+//  Created by Petre Vane on 24/07/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContainerView: View {
+        
+    var body: some View {
+        VStack {
+            BreakfastView()
+        }
+    }
+}
+
+struct ContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+    }
+}
---
+++ MealPlan/Extensions/View+Extension.swift
@@ -0,0 +1,11 @@
+//
+//  View+Extension.swift
+//  MealPlan
+//
+//  Created by Petre Vane on 24/07/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import SwiftUI
+
+
