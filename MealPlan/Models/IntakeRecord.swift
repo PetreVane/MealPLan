0ae648914e7ddc9890cb5a206e9fd139306d9050
Petre Vane <petre.vane@gmail.com>
2020-07-30 10:47:18 +0200

    Remodel food intake

--- MealPlan/Models/IntakeRecord.swift
+++ MealPlan/Models/IntakeRecord.swift
@@ -12,34 +12,64 @@ import Foundation
 struct IntakeRecord {
     
     var date = Date()
-    var timeOfDay: String // eg. breakfast or dinner
+    var breakfast: Breakfast
+    var lunch: Lunch
+    var dinner: Dinner
+}
+
+struct Breakfast {
+    isCompleted: Bool
     var proteins: Proteins
     var grains: Grains
     var fruits: Fruits
-    var vegetables: Vegetables?
-    var salad: String?
-    var fats: Fats?
-    
-    
-    /// Contains a selection of common proteins consumed during the day and a dictionary that holds user selected proteins and quantity
-    struct Proteins {
-    
-    }
-    
-    struct Grains {
-        
-    }
-    
-    struct Fruits {
-        
-    }
-    
-    struct Vegetables {
-    
-    }
-    
-    struct Fats {
-        
-    }
 }
+
+struct Lunch {
+    isCompleted: Bool
+    var proteins: Proteins
+    var grains: Grains
+    var vegetables: Vegetables
+    var fruits: Fruits
+    var fats: Fats
+}
+
+struct Dinner {
+    isCompleted: Bool
+    var proteins: Proteins
+    var grains: Grains
+    var vegetables: Vegetables
+    var fruits: Fruits
+    var fats: Fats
+}
+
+struct Proteins {
+    var elements: [String]
+    var quantity: Int
+    var units: String
+}
+
+struct Grains {
+    var elements: [String]
+    var quantity: Int
+    var units: String
+}
+
+struct Fruits {
+    var elements: [String]
+    var quantity: Int
+    var units: String
+}
+
+struct Vegetables {
+    var elements: [String]
+    var quantity: Int
+    var units: String
+}
+
+struct Fats {
+    var elements: [String]
+    var quantity: Int
+    var units: String
+}
+
 
